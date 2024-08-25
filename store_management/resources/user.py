47b507f8b054b30e921cb00a83e528f0af374c05
asileshi <abebesileshi828@gmail.com>
2024-08-25 04:11:39 +0300

    reresh token implemented

--- store_management/resources/user.py
+++ store_management/resources/user.py
@@ -1,8 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import jwt_required, get_jwt
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required, get_jwt
 
 from db import db
 from blocklist import BLOCKLIST
@@ -36,8 +35,9 @@ class UserLogin(MethodView):
         ).first()
 
         if user and pbkdf2_sha256.verify(user_data["password"],user.password):
-            access_token = create_access_token(identity=user.id)
-            return {"access_token":access_token}
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(identity=user.id)
+            return {"access_token":access_token, "refresh_token":refresh_token}
         abort(401, message="Invalide credentials.")  
 
 @blp.route("/user/<int:user_id>")
@@ -46,7 +46,7 @@ class User(MethodView):
     def get(self, user_id):
         user = UserModel.query.get_or_404(user_id)
         return user
-    @jwt_required()
+    @jwt_required(fresh=True)
     def delete(self, user_id):
         user = UserModel.query.get_or_404(user_id)
         db.session.delete(user)
@@ -62,3 +62,11 @@ class UserLogout(MethodView):
         return {"message":"Successfully logged out."}
 
  
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {"access_token": new_token}
+    
