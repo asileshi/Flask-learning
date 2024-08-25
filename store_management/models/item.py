4b457b6586625e92d9734b8c6d5897448a9c2d6e
asileshi <abebesileshi828@gmail.com>
2024-08-25 15:22:34 +0300

    running the app using gunicorn

--- store_management/models/item.py
+++ store_management/models/item.py
@@ -7,6 +7,6 @@ class ItemModel(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     price = db.Column(db.Float, nullable=False)  # Removed precision argument
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'), nullable=False)
-
+    description = db.Column(db.String)
     store = db.relationship('StoreModel', back_populates='items')
     tags = db.relationship('TagModel', back_populates="items", secondary="items_tags")
