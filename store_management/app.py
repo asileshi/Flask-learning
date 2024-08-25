f0c99078882cc6108b7ee15334d1f097a3ef3fb8
asileshi <abebesileshi828@gmail.com>
2024-08-25 03:17:15 +0300

    added claims

--- store_management/app.py
+++ store_management/app.py
@@ -19,6 +19,12 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "46832766446820110000213922374257060926"
     jwt = JWTManager(app)
     
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        #check the database if the user is a admin
+        if identity == 1:
+            return {"is_admin":True}
+
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return (
--- store_management/resources/item.py
+++ store_management/resources/item.py
@@ -3,7 +3,7 @@ from db import db
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt
 
 from schemas import ItemSchema, ItemUpdateSchema
 
@@ -17,6 +17,9 @@ class Item(MethodView):
     
     @jwt_required()
     def delete(self, item_id):
+        jwt = get_jwt()
+        if not jwt.get("is_admin"):
+            abort(401, message="Admin previlage required")
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
         db.session.commit()
